03b4be980ab3ac2ad50bf532284bbddd0ce08cdd
lilkho <mkhondker@stuy.edu>
2019-03-06 14:35:39 -0500

    starting work

--- USACO.java
+++ USACO.java
@@ -1,5 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class USACO {
-  public static int bronze(String filename){
+  public static int bronze(String filename) throws FileNotFoundException{
+    // array of 6x6 squares, making the pasture
+    int[][] pasture;
+    // row, column, elevation, down
+    int r,c,e,n;
+    //getting input file
+    File input = new File(filename);
+    Scanner lines = new Scanner(input);
+    //getting first line of 
     return 0;
   };
+  public static void main(String[] args) {
+    String filename = args[0];
+    try {
+      System.out.println(bronze(filename));
+    } catch(FileNotFoundException F) {
+      System.out.println(filename+" not found.");
+    }
+  }
 }
