30e52e585bc947b5e3a561b6aa9bd1350cc084fa
lilkho <mkhondker@stuy.edu>
2019-03-07 14:13:33 -0500

    stuff

--- USACO.java
+++ USACO.java
@@ -7,11 +7,30 @@ public class USACO {
     // array of 6x6 squares, making the pasture
     int[][] pasture;
     // row, column, elevation, down
-    int r,c,e,n;
+    int r=0;int c=0;int e=0;int d=0;
     //getting input file
     File input = new File(filename);
     Scanner lines = new Scanner(input);
-    //getting first line of 
+    String currentLine = lines.nextLine();
+    System.out.println(currentLine);
+    //getting r,c,e,n values
+    int n=0;String num="";
+    for (int i=0;i<currentLine.length();i++) {
+      if (currentLine.charAt(i)!=' ') {
+        num+=currentLine.charAt(i);
+      } else {
+        if (n==0) r=Integer.parseInt(num);
+        if (n==1) c=Integer.parseInt(num);
+        if (n==2) e=Integer.parseInt(num);
+        num="";
+        n++;
+      }
+    }
+    if (n==3) n=Integer.parseInt(num);
+    System.out.println(r);
+    System.out.println(c);
+    System.out.println(e);
+    System.out.println(d);
     return 0;
   };
   public static void main(String[] args) {
