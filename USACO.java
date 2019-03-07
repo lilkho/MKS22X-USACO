e2509561cf0d8961649f7c7b0f0a043200da009a
lilkho <mkhondker@stuy.edu>
2019-03-07 14:41:23 -0500

    adjusting method of getting input values

--- USACO.java
+++ USACO.java
@@ -12,25 +12,26 @@ public class USACO {
     File input = new File(filename);
     Scanner lines = new Scanner(input);
     String currentLine = lines.nextLine();
-    System.out.println(currentLine);
     //getting r,c,e,n values
-    int n=0;String num="";
-    for (int i=0;i<currentLine.length();i++) {
-      if (currentLine.charAt(i)!=' ') {
-        num+=currentLine.charAt(i);
-      } else {
-        if (n==0) r=Integer.parseInt(num);
-        if (n==1) c=Integer.parseInt(num);
-        if (n==2) e=Integer.parseInt(num);
-        num="";
-        n++;
-      }
+    String[] nums = currentLine.split(" ",4);
+    for (int i=0;i<nums.length;i++) {
+      if (i==0) r=Integer.parseInt(nums[i]);
+      if (i==1) c=Integer.parseInt(nums[i]);
+      if (i==2) e=Integer.parseInt(nums[i]);
+      if (i==3) d=Integer.parseInt(nums[i]);
     }
-    if (n==3) n=Integer.parseInt(num);
-    System.out.println(r);
-    System.out.println(c);
-    System.out.println(e);
-    System.out.println(d);
+    //getting pasture
+    pasture = new int[r][c];
+    currentLine = lines.nextLine();
+    nums = currentLine.split(" ",c);
+    for (int i=0;i<r;i++) {
+      System.out.println(currentLine);
+      for (int j=0;j<c;j++) {
+        pasture[i][j]=Integer.parseInt(nums[j]);
+        System.out.println(pasture[i][j]);
+      }
+      currentLine = lines.nextLine();
+    }
     return 0;
   };
   public static void main(String[] args) {
